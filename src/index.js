089afcf86906f2c41e4d432ed7311dd191e7500a
oreol Noumodong <oreolnoumodong@gmail.com>
2021-12-26 01:17:21 +0100

    add some packages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'animate.css';
-import App from './App';
+import App from './components/App';
 
 ReactDOM.render(
     <App />,
